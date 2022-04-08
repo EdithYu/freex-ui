c040812de6be78638935ae1706f64c0ece03a024
zqyu <zqyu@streamax.com>
2022-04-08 16:02:01 +0800

    add: 空项目

---
+++ webpack.xantd.config.js
@@ -0,0 +1,98 @@
+const path = require('path');
+const toml = require('toml');
+const yaml = require('yamljs');
+const json5 = require('json5');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: './src/index.ts',
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ],
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'lib'),
+  },
+  resolve: {
+    alias: {
+      '@': resolve('src'),
+    },
+    extensions: [
+      '.js', '.jsx', '.ts', '.tsx'
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+              plugins: [
+                "@babel/plugin-proposal-class-properties",
+              ]
+            }
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        use:[
+          {
+            loader:'ts-loader',
+            options:{
+              transpileOnly: true,
+            }
+          }
+        ],
+        exclude:/node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.less$/i,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.toml$/i,
+        type: 'json',
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/i,
+        type: 'json',
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.json5$/i,
+        type: 'json',
+        parser: {
+          parse: json5.parse,
+        },
+      },
+    ],
+  },
+};
